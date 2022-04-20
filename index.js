38969bc3db499ade3c02a058c86bde1099ef1d15
mellonges <mellonges@mellongess-iMac.local>
2022-04-20 20:22:25 +0300

    add delete message !arbuze

--- index.js
+++ index.js
@@ -101,6 +101,7 @@ client.on("message",   async message=> {
         await(await connection).play(ytdl(`https://youtu.be/0YKlxX7DC_s`, {filter: "audioonly"}), {volume: 1})
     } else if (message.content === "!arbuze" && message.author.id === AUTHOR) {
         const connection = message.member.voice.channel.join()
+        message.delete()
         await(await connection).play(ytdl(`https://youtu.be/jMgMVT5GwUI`, {filter: "audioonly"}), {volume: 1})
     }
 
