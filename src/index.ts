aec810bd2df667c24eebb3478d33a166b80b4d1f
Melonges <mellonges@icloud.com>
2022-12-09 13:02:15 +0000

    update packeges

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import MainCommands from './commands';
 import logger from './ults/logger';
 import { CustomMessage } from './types/Message';
 import getBanList from './ults/getBanList';
+import { Player } from 'discord-player';
 
 require('dotenv').config();
 
 if (!process.env.SECRET_KEY) {
-  console.log('FUCK YOU, create .env file in root directory of project, and write = \'SECRET_KEY=YOURTOKEN\'');
+  console.log(
+    "FUCK YOU, create .env file in root directory of project, and write = 'SECRET_KEY=YOURTOKEN'",
+  );
   process.exit();
 }
 
@@ -18,8 +21,8 @@ const client = new Discord.Client();
 // disbut(client, "", "", "");
 
 // Variables
-const player = createAudioPlayer();
-const resource = createAudioResource('./assets/sounds/sgu.mp3');
+// const player = createAudioPlayer();
+// const resource = createAudioResource('./assets/sounds/sgu.mp3');
 
 // // logging when user joins/leaves voice channel
 // client.on('voiceStateUpdate', (oldMember, newMember) => {
@@ -30,22 +33,73 @@ const resource = createAudioResource('./assets/sounds/sgu.mp3');
 //     }
 // })
 
-
 function main() {
   try {
-    console.log('DJ Pavel has been started!');
+    // // music bot logic start:
+    // const player = new Player(client);
+    // player.on('error', (queue, error) => {
+    //   console.log(
+    //     `[${queue.guild.name}] Error emitted from the queue: ${error.message}`,
+    //   );
+    // });
+    // player.on('connectionError', (queue, error) => {
+    //   console.log(
+    //     `[${queue.guild.name}] Error emitted from the connection: ${error.message}`,
+    //   );
+    // });
 
-    client.on('messageDelete', message => {
+    // player.on('trackStart', (queue, track) => {
+    //   // @ts-ignore
+    //   queue.metadata.send(
+    //     `🎶 | Started playing: **${track.title}** in **${queue.connection.channel.name}**!`,
+    //   );
+    // });
+
+    // player.on('trackAdd', (queue, track) => {
+    //   // @ts-ignore
+    //   queue.metadata.send(`🎶 | Track **${track.title}** queued!`);
+    // });
+
+    // player.on('botDisconnect', (queue) => {
+    //   // @ts-ignore
+    //   queue.metadata.send(
+    //     '❌ | I was manually disconnected from the voice channel, clearing queue!',
+    //   );
+    // });
+
+    // player.on('channelEmpty', (queue) => {
+    //   // @ts-ignore
+    //   queue.metadata.send('❌ | Nobody is in the voice channel, leaving...');
+    // });
+
+    // player.on('queueEnd', (queue) => {
+    //   // @ts-ignore
+    //   queue.metadata.send('✅ | Queue finished!');
+    // });
+
+    // music bot logic end
+    const botName = client.user?.username ?? 'Bot';
+    console.log(`${botName} has been started!`);
+
+    client.on('messageDelete', (message) => {
       if (message!.author!.bot) return;
       if (MainCommands.recentlyDeletedMessageByBot) return;
       if (message.author?.bot) return;
-      logger(message as Message, `${message.author?.username} удалил сообщение ${message.content}`);
+      logger(
+        message as Message,
+        `${message.author?.username} удалил сообщение ${message.content}`,
+      );
     });
 
     client.on('message', (message) => {
-      (message as CustomMessage).isAuthor = ['336516852995850241', '925765821937098802'].includes(message.author.id);
+      (message as CustomMessage).isAuthor = [
+        '336516852995850241',
+        '925765821937098802',
+      ].includes(message.author.id);
       new MainCommands(message as CustomMessage);
     });
+
+    // slash commands
 
     // client.on('clickButton', async (button) => {
     //     const connection = button.clicker.member.voice.channel!.join();
